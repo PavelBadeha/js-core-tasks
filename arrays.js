48722d7002b7d385d58f314972d66b2be661e3f3
Pavel_Badzekha@epam.com <Pavel_Badzekha@epam.com>
2020-01-20 13:38:59 +0300

    Task for arrays

---
+++ arrays.js
@@ -0,0 +1,71 @@
+
+let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+// добавляем новое значение в "копию"
+let shoppingCart = fruits;
+shoppingCart.push("Банан");
+
+// что в fruits?
+alert( fruits.length ); // 4
+
+let styles = ["Джаз","Блюз"]
+styles.push("Рок-н-ролл")
+
+styles[Math.floor(styles.length-1)/2] = "Классика"
+alert(styles.shift())
+alert(styles.unshift("Рэп","Рэгги"))
+
+let arr = ["a", "b"];
+
+arr.push(function() 
+{
+  alert( this );
+})
+
+arr[2](); // returns arr.toString()
+
+function sumInput() 
+{
+    let sum = 0;
+    let arr = [];
+   while(true)
+   {
+       let value = +prompt("Input a number");
+       if(value || value == 0  && !Button.IsPressed)   
+       {
+            arr.push(value) 
+       }
+       else
+       {
+           break;
+       }        
+   } 
+
+   for(var val of arr )
+   {
+       sum += val;
+   }
+    return sum;
+}
+
+arr = [1, -2, 3, 4, -9, 6,]
+
+function getMaxSubSum(arr) 
+{
+    let tempSum = 0;
+    let maxSum = 0;
+    for(var value of arr)
+    {
+        if(tempSum < tempSum + value)
+        {
+            tempSum += value;
+            maxSum = Math.max(maxSum,tempSum)
+        }
+        else
+        {
+           tempSum = 0;
+        }
+    }
+    return maxSum;
+}
+console.log(getMaxSubSum(arr));
