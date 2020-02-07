8b6f0d5de80b372191568f811d61f52a80ce8f2f
Pavel Badzekha <Pavel_Badzekha@epam.com>
2020-02-07 15:15:21 +0300

    task for protractor

---
+++ protractor/spec.js
@@ -0,0 +1,25 @@
+describe('Protractor Demo App',function ()
+{
+  let homeButton = element(by.xpath('//div[@class = "orb-nav-section orb-nav-blocks"]/a'))
+  let inputSearch = element(by.id('orb-search-q'))
+  let urlBBC = "https://www.bbc.com/"
+  let welcomeElement = element(by.xpath('//div[@id = "page"]/section[@class = "module module--header"]/h2'))
+
+  browser.ignoreSynchronization = true
+  browser.get(urlBBC)
+
+  it('Shoud be a BBC', function()
+  {
+    expect(homeButton.getAttribute('href')).toEqual(urlBBC)
+  })
+
+  it('Shoud be a text',function()
+  {
+    expect(inputSearch.getAttribute('type')).toEqual("text")
+  })
+
+  it('Shoud be welcome banner',function()
+  {
+    expect(welcomeElement.getText()).toContain("Welcome to BBC.com")
+  })
+})
