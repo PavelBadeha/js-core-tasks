e45c8ccc8435d40b48f3e0acabc57e8562295c51
Pavel Badzekha <Pavel_Badzekha@epam.com>
2020-02-07 16:54:46 +0300

    Added 3 config for different browsers

---
+++ protractor/confForChrome.js
@@ -0,0 +1,12 @@
+// conf.js
+exports.config = 
+{
+    directConnect: true,
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    capabilities:
+    {
+        browserName:"chrome"
+    }
+}
