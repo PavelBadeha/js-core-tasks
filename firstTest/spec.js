60c7899c7322e235350788a8b79f450f576f05ec
Pavel Badzekha <Pavel_Badzekha@epam.com>
2020-03-11 19:17:41 +0300

    Debugin'

--- firstTest/spec.js
+++ firstTest/spec.js
@@ -1,30 +1,30 @@
 import {YandexSearchPage} from "./PageObjects/YandexSearchPage.js";
+import {YandexLocationPage} from './PageObjects/YandexLocationPage.js';
 
 describe("Test",function()
-{
+{   
     browser.ignoreSynchronization = true;
     browser.get("https://yandex.by/");
 
-    const yandexSearchPage = new YandexSearchPage(browser);
-
+    const yandexSearchPage = new YandexSearchPage();
+    let yandexLocationPage =  new YandexLocationPage();
     it("My first test",async function()
     {
-        let yandexLocationPage = await yandexSearchPage.navigateToLocationPage();
+        yandexLocationPage = await yandexSearchPage.navigateToLocationPage();
+
         await yandexLocationPage.setLocation("Лондон");
         await yandexLocationPage.backToSearchPage();
-    
         let yandexMoreButtonPage = await yandexSearchPage.navigateToYandexMoreButtonPage()
         const londonMoreItems = await yandexMoreButtonPage.elementsUnderMoreButton.getText();
 
         yandexLocationPage = await yandexSearchPage.navigateToLocationPage();
-        await yandexLocationPage.setLocation("Париж");
+        await yandexLocationPage.setLocation("Paris");
         await yandexLocationPage.backToSearchPage();
 
         yandexMoreButtonPage = await yandexSearchPage.navigateToYandexMoreButtonPage()
         const parisMoreItems = await yandexMoreButtonPage.elementsUnderMoreButton.getText();
 
         expect(londonMoreItems).toEqual(parisMoreItems);
+        browser.sleep(4000);
     })
-   
-
 })
