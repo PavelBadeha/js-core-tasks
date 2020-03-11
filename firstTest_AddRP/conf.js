e795e99c9187e43ae36266a9695288ac505e5a4f
Pavel Badzekha <Pavel_Badzekha@epam.com>
2020-03-11 19:32:13 +0300

    Debugin' for Jenkins

--- firstTest_AddRP/conf.js
+++ firstTest_AddRP/conf.js
@@ -7,6 +7,7 @@ exports.config =
 {
     seleniumAddress:"http://localhost:4444/wd/hub",
     specs:['spec.js'],
+    directConnection: true,
     onPrepare: function () 
     {
         require("babel-core/register")({presets: ["es2015"]});
